8cd139b1c5cf9cde3d5bf33a6071e5a1b63451a1
Lisa Lee <snickers495@gmail.com>
2018-02-14 10:40:28 -0600

    added a key and gitignore file, as well as beginning map

--- browser/components/Navbar.jsx
+++ browser/components/Navbar.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-
+import { NavLink, withRouter } from 'react-router-dom';
+//fix search bar, two fields, find near ...
 export class Navbar extends React.Component {
   render() {
     return(
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo center">Logo</a>
+          <a href="#" className="brand-logo center">Foodie</a>
             <ul id="nav-mobile" className="left hide-on-med-and-down">
-              <li><a href="sass.html">Sass</a></li>
-              <li><a href="badges.html">Components</a></li>
-              <li><a href="collapsible.html">JavaScript</a></li>
+              <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+              <li><NavLink to="/favorites" activeClassName="active">Favorites</NavLink></li>
+              <li><NavLink to="/logout" activeClassName="active">Logout</NavLink></li>
             </ul>
             <form>
+              <div className="input-field">
+                <input id="search" type="search" required/>
+                <label className="label-icon" for="search"><i class="material-icons">search</i></label>
+                <i className="material-icons">close</i>
+              </div>
               <div className="input-field">
                 <input id="search" type="search" required/>
                 <label className="label-icon" for="search"><i class="material-icons">search</i></label>
