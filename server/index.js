ed18ccf00da2815b50ec08da358e10cfc5a2031f
Lisa Lee <snickers495@gmail.com>
2018-02-14 19:00:42 -0600

    working on Login, added passport info to the sessions router

--- server/index.js
+++ server/index.js
@@ -6,6 +6,41 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const { User } = require('./db/models');
 
+app.use(session({
+  // this mandatory configuration ensures that session IDs are not predictable
+  secret: 'l33sa', // or whatever you like
+  // this option says if you haven't changed anything, don't resave. It is recommended and reduces session concurrency issues
+  resave: false,
+  // this option says if I am new but not modified still save
+  saveUninitialized: true
+})); // this gives us req.session!
+
+app.use(passport.initialize()); // middleware required to initialize Passport
+app.use(passport.session()); // hooks into the persistent sessions we are using
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id)
+    .then(function (user) {
+      done(null, user);
+    })
+    .catch(done);
+});
+
+app.use(function (req, res, next) {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('counter', ++req.session.counter); // increment THEN log
+  next(); // needed to continue through express middleware
+});
+
+app.use(function (req, res, next) {
+  console.log('SESSION USER: ', req.user && req.user.id);
+  next();
+});
+
 app.use(volleyball);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
