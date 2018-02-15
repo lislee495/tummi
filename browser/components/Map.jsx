10a0d942e179d03dd83c93f0ff095430a01df08f
Lisa Lee <snickers495@gmail.com>
2018-02-14 18:46:46 -0600

    worked on Map

--- browser/components/Map.jsx
+++ browser/components/Map.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import mapboxgl from 'mapbox-gl';
+
 class Map extends React.Component {
   render(){
+    mapboxgl.accessToken = config.MAPBOX_KEY;
+    const map = new mapboxgl.Map({
+        container: '<App/>',
+        style: 'mapbox://styles/mapbox/streets-v9'
+    });
     return (
-      
+      {map}
     )
   }
 }
