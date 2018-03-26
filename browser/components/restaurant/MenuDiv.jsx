b85930ac65445c454aec87d5dc1259fb22df2a63
Lisa Lee <lisa_lee@alumni.brown.edu>
2018-03-26 13:41:40 -0500

    testing modal for cart

--- browser/components/restaurant/MenuDiv.jsx
+++ browser/components/restaurant/MenuDiv.jsx
@@ -1,16 +1,58 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {addDishToCart, addRestaurantToCart, favoriteDish, fetchFavorites} from '../../redux'
+import {addDishToCart, clearCart, addRestaurantToCart, favoriteDish, fetchFavorites} from '../../redux'
 import Alert from 'react-s-alert';
+import Modal from 'react-modal';
+
+
+const styles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+}
+Modal.setAppElement('#main');
 class MenuDiv extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      modalIsOpen: false
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
   componentDidMount(){
     this.props.fetchFavorites(this.props.currentUser)
   }
   render() {
-  const {dish, handleClick, restaurant, currentUser, handleFavorite, favoriteDishes} = this.props
+  const {dish, handleClick, restaurant, currentUser, handleFavorite, favoriteDishes, clearCart} = this.props
     return(
       <li>
         <div className="dish-div shadow" style={{cursor: "pointer"}} onClick={(event)=>{
+          if (dish.restaurant.name !== restaurant.name) {
+            return(<Modal
+              isOpen={true}
+              onAfterOpen={this.afterOpenModal}
+              onRequestClose={this.closeModal}
+              style={styles}
+            >
+          <h5>Your cart already has items from a different restaurant. Clear cart?</h5>
+          <button onClick={clearCart()}>Yes</button>
+          <button onClick={this.closeModal()}>No</button>
+        </Modal>)
+          } else {
           Alert.info('Added dish', {
             timeout: 2000,
             position: 'top-right',
@@ -18,13 +60,13 @@ class MenuDiv extends React.Component {
             beep: false,
             offset: 100
         });
-          handleClick(dish, restaurant)}}>
+          handleClick(dish, restaurant)}}}> 
           <h6>{dish.name}</h6><br/>
           <em>{dish.category.join(", ")}</em><br/>
           ${dish.price} 
           {favoriteDishes.find(item => item.dish_id === dish.id) ? "Favorited" : 
          <button className="gen-btn" 
-         onClick={(event)=>{
+         onClick={(event) =>{
            event.stopPropagation();
            handleFavorite(dish, restaurant, currentUser)}}>Favorite</button>}
         </div>
@@ -37,11 +79,12 @@ const mapStateToProps = (state, ownProps) => ({
   currentUser: state.currentUser,
   favoriteDishes: state.restaurants.favorites.filter(ele => ele.restaurant_id === ownProps.restaurant.id)
 })
-const mapDispatchToProps = (dispatch)=> ({
+const mapDispatchToProps = (dispatch) => ({
   handleClick: (dish, restaurant) => {
     dispatch(addDishToCart(dish))
     dispatch(addRestaurantToCart(restaurant))},
   handleFavorite: (dish, restaurant, currentUser) => dispatch(favoriteDish({dish, restaurant, currentUser})),
-  fetchFavorites: (currentUser) => dispatch(fetchFavorites(currentUser))
+  fetchFavorites: (currentUser) => dispatch(fetchFavorites(currentUser)),
+  clearCart: () => dispatch(clearCart())
 })
 export default connect(mapStateToProps, mapDispatchToProps)(MenuDiv);
--- browser/redux/cart.js
+++ browser/redux/cart.js
@@ -1,8 +1,4 @@
 import axios from 'axios'
-import config from '../config'
-import Promise from 'bluebird'
-import { browserHistory } from 'react-router'
-
 
 /* -----------------    ACTION TYPES    ------------------ */
 
@@ -11,7 +7,7 @@ const CLEAR_CART = "CLEAR_CART"
 const SHOW_CART = "SHOW_CART"
 const ADD_RESTAURANT = "ADD_RESTAURANT"
 const REMOVE_ITEM = "REMOVE_ITEM"
-
+const RESET_CART = "RESET_CART"
 
 /* ------------     ACTION CREATORS      ------------------ */
 
@@ -20,6 +16,7 @@ export const addRestaurantToCart = restaurant => ({type: ADD_RESTAURANT, restaur
 export const clearCart = () => ({ type: CLEAR_CART})
 export const showCart = () => ({type: SHOW_CART})
 export const removeItem = dish => ({ type: REMOVE_ITEM, dish})
+export const resetCart = () => ({ type: RESET_CART})
 
 /* ------------          REDUCER         ------------------ */
 
@@ -38,14 +35,14 @@ export default function reducer (cart = {
       } else {
       return Object.assign({}, cart, {dishes: [...cart.dishes, action.payload], total: cart.total + action.payload.dish.price})}
     case CLEAR_CART:
-      return Object.assign({}, cart, {dishes: [], restaurant: {}})
+      return Object.assign({}, cart, {dishes: [], restaurant: {}, total: 0})
     case SHOW_CART:
       return Object.assign({}, cart, {showCart: !cart.showCart})
     case ADD_RESTAURANT:
       return Object.assign({}, cart, {restaurant: action.restaurant})
     case REMOVE_ITEM:
       var dishIndex = cart.dishes.findIndex(ele => ele.dish.id === action.dish.id)
-      cart.dishes[dishIndex].quantity -= 1; 
+      cart.dishes[dishIndex].quantity -= 1
       return Object.assign({}, cart, {dishes: cart.dishes.filter(ele => ele.quantity !== 0), total: cart.total - action.dish.price})
     default:
       return cart;
